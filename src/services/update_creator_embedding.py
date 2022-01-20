35e9f13e6da2e0464c164275b1d163f8e86f23a7
Vladyslav Matsiiako <matsiiako@gmail.com>
2022-01-19 21:51:52 -0500

    update_creator_embeddings adjusted for new embeddings format

--- src/services/update_creator_embedding.py
+++ src/services/update_creator_embedding.py
@@ -34,57 +34,16 @@ def pickle_word_embeddings():
         pickle.dump(embeddings_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
-def query_transcriptions(user_id):
-    """
-    Get all the audios for a certain user where transcription exists and concatenate their transcfriotions 
-    """
-    audios = db.audios.find({"user": ObjectId(user_id), "transcription": {"$exists":1}})
-    audio_transcriptions = ''
-    for audio in audios:
-        audio_transcriptions += " " + audio['transcription']
-    
-    return audio_transcriptions
-
-
-def delete_stopwords(audio_transcriptions):
-    """
-    Delete stopwords from the audio transcriptions (a, the, and, etc.)
-    """
-    with open(DIR_PATH + '/word_embeddings/stopwords.pickle', 'rb') as handle:
-        stopwords = pickle.load(handle)
-    transcriptions_without_stop_words = [word for word in audio_transcriptions.split() if not word in stopwords]
-    return transcriptions_without_stop_words
-
-
-def load_embeddings():
-    """
-    Load teh previously pickled embeddings
-    """
-    with open(DIR_PATH + '/word_embeddings/embeddings_twitter.pickle', 'rb') as handle:
-        embeddings_dict = pickle.load(handle)
-    return embeddings_dict
-
-
-def calculate_embedding(embeddings_dict, transcriptions_without_stop_words):
-    """
-    Calculate creator embeddings by putting them through word embeddings or making them 0's if those don't exist
-    """
-    creator_embedding = np.mean([embeddings_dict[audio] for audio in transcriptions_without_stop_words if audio in embeddings_dict], axis=0).tolist()
-    if creator_embedding != creator_embedding:
-        creator_embedding = [0] * 50
-    return creator_embedding
-
-
 def generate_creator_embedding(user_id):
-    embeddings_dict = load_embeddings()
-
-    audio_transcriptions = query_transcriptions(user_id)
-
-    transcriptions_without_stop_words = delete_stopwords(audio_transcriptions)
-
-    creator_embedding = calculate_embedding(embeddings_dict, transcriptions_without_stop_words)
-
-    return creator_embedding
+    """
+    Queries wordEmbeddings for every audio that a user has recorded and take an average of those. 
+    If there is no audios, assigns a 0 embedding. 
+    """
+    embedding = np.mean([audio["wordEmbedding"] for audio in list(db.audios.find({"user": ObjectId(user_id), "wordEmbedding": {"$exists":1}}, {"wordEmbedding": 1}))], axis=0).tolist()
+    print(embedding)
+    if embedding != embedding:
+        embedding = [0] * 25
+    return embedding
 
 
 def push_embedding_to_db(user_id, creator_embedding):
@@ -96,3 +55,46 @@ def push_embedding_to_db(user_id, creator_embedding):
         {"$set": {"creatorEmbedding": creator_embedding}},
         upsert=True
     )
+
+
+# THESE FUNCTIONS COULD BE USEFUL IN FUTURE IF WE WANT TO EASILY CHANGE THE CREATOR EMBEDDINGS WITHOUT CHANGING WORDEMBEDDING OF EACH AUDIO
+
+# def query_transcriptions(user_id):
+#     """
+#     Get all the audios for a certain user where transcription exists and concatenate their transcfriotions 
+#     """
+#     audios = db.audios.find({"user": ObjectId(user_id), "transcription": {"$exists":1}})
+#     audio_transcriptions = ''
+#     for audio in audios:
+#         audio_transcriptions += " " + audio['transcription']
+    
+#     return audio_transcriptions
+
+
+# def delete_stopwords(audio_transcriptions):
+#     """
+#     Delete stopwords from the audio transcriptions (a, the, and, etc.)
+#     """
+#     with open(DIR_PATH + '/word_embeddings/stopwords.pickle', 'rb') as handle:
+#         stopwords = pickle.load(handle)
+#     transcriptions_without_stop_words = [word for word in audio_transcriptions.split() if not word in stopwords]
+#     return transcriptions_without_stop_words
+
+
+# def load_embeddings():
+#     """
+#     Load teh previously pickled embeddings
+#     """
+#     with open(DIR_PATH + '/word_embeddings/embeddings_twitter.pickle', 'rb') as handle:
+#         embeddings_dict = pickle.load(handle)
+#     return embeddings_dict
+
+
+# def calculate_embedding(embeddings_dict, transcriptions_without_stop_words):
+#     """
+#     Calculate creator embeddings by putting them through word embeddings or making them 0's if those don't exist
+#     """
+#     creator_embedding = np.mean([embeddings_dict[audio] for audio in transcriptions_without_stop_words if audio in embeddings_dict], axis=0).tolist()
+#     if creator_embedding != creator_embedding:
+#         creator_embedding = [0] * 25
+#     return creator_embedding
