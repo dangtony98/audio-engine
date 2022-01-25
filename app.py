3256c37888e5a25a0d4446218e363fd66baa10d6
Vladyslav Matsiiako <matsiiako@gmail.com>
2022-01-24 22:29:36 -0500

    path issue solved

--- app.py
+++ app.py
@@ -14,12 +14,12 @@ app = Flask(__name__)
 #     client = MongoClient(os.environ.get("MONGO_DEVELOPMENT_URI"), tls=True, tlsAllowInvalidCertificates=True)
 #     db = client.audio_testing
 
-from services.feed.get_discover import *
+from src.services.feed.get_discover import *
 from src.services.transcribe import *
 from src.services.get_prof_recs import *
 from src.services.retrain_prof_recommender import *
 from src.services.update_creator_embedding import *
-from services.search.search import *
+from src.services.search.search import *
 from src.middleware.middleware import *
 from tasks import background_transcribe
 
